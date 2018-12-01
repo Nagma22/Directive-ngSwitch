7d5571c2f7899435407df7bf020e83921018cd4c
Nagma22 <nagmashima7@gmail.com>
2018-12-01 13:05:56 +0000

    Commiting Wth Directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Item } from './item';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'App-ngSwitch-Drective';
+
+  num : number =0;
+
+  showMe : boolean;
+
+  items : Item[] = [
+        {name:"One", val:1},
+        {name:"Two", val:2},
+        {name:"Three", val:3},
+        {name:"Four", val:4},
+  ];
+
+  selectedValue: string= "One";
 }
